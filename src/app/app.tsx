9d69ff8b85811b6453db8e9983859166a01c5702
niknov80 <n.novozhilov80@gmail.com>
2024-12-02 22:16:40 +0300

    3.19. Маршрут по тайным тропам

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,19 +1,20 @@
 import { JSX } from 'react';
-import MainPage from '../../pages/main-page/main-page';
+import MainPage from '../pages/main-page/main-page.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { AppRoute, AuthorizationStatus } from '../../const';
-import LoginPage from '../../pages/login-page/login-page';
-import OfferPage from '../../pages/offer-page/offer-page';
-import NotFoundPage from '../../pages/not-found-page/not-found-page';
-import PrivateRoute from '../private-route/private-route';
-import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import { AppRoute, AuthorizationStatus } from '../const.ts';
+import LoginPage from '../pages/login-page/login-page.tsx';
+import OfferPage from '../pages/offer-page/offer-page.tsx';
+import NotFoundPage from '../pages/not-found-page/not-found-page.tsx';
+import PrivateRoute from '../components/private-route/private-route.tsx';
+import FavoritesPage from '../pages/favorites-page/favorites-page.tsx';
 
 type AppProps = {
   placeCardCount: number;
+  authStatus: AuthorizationStatus;
 }
 
-function App({ placeCardCount }: AppProps): JSX.Element {
+function App({ placeCardCount, authStatus }: AppProps): JSX.Element {
   return (
     <HelmetProvider>
       <BrowserRouter>
@@ -22,7 +23,7 @@ function App({ placeCardCount }: AppProps): JSX.Element {
           <Route path={ AppRoute.Login } element={ <LoginPage /> } />
           <Route path={ AppRoute.Offer } element={ <OfferPage /> } />
           <Route path={ AppRoute.Favorites } element={
-            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <PrivateRoute authorizationStatus={ authStatus }>
               <FavoritesPage />
             </PrivateRoute>
           }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
-import { Setting } from './const';
+import App from './app/app';
+import { AuthorizationStatus, Setting } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App placeCardCount={ Setting.PlaceCardCount } />
+    <App placeCardCount={ Setting.PlaceCardCount } authStatus={ AuthorizationStatus.Auth } />
   </React.StrictMode>
 );
